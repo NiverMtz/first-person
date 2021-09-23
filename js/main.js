181d52ec93df170800efe99cfb479f6319304e30
NiverMtz <nivermartinezmed@gmail.com>
2021-09-23 04:40:56 -0500

    working on loader

--- js/main.js
+++ js/main.js
@@ -20,11 +20,12 @@ async function getData(year, month, day) {
     fetch(`https://api.nasa.gov/planetary/apod?api_key=BAKO9hbFf4bJYTTXAB0udACfhoA9rYw9VeNoCfBy&start_date=${year}-${month}-${day}&end_date=${year}-${month}-${day  }`)
     const json = await response.json();
     const { date, explanation, title, url } = json[0];
+    document.getElementById("apod-card").setAttribute("class", "card text-dark")
     document.getElementById("apod-url").setAttribute("src", url)
     _date = document.getElementById("apod-date")
-    _date.textContent += date;
+    _date.textContent += "Date: " + date;
     _explanation = document.getElementById("apod-explanation")
-    _explanation.textContent += explanation;
+    _explanation.textContent += "Explanation: " + explanation;
     _title = document.getElementById("apod-title")
     _title.textContent += title
 }
